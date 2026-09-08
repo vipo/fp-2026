5b5a3ecd967348b3fdaab8c5c167f7a8ced02a29
ViPo <pi@homedir.eu>
2026-09-08 17:29:30 +0300

    Lesson01

--- src/Lessons/Lesson01.hs
+++ src/Lessons/Lesson01.hs
@@ -1,10 +1,52 @@
 module Lessons.Lesson01 where
 
-ff :: Int
-ff = 42
+i :: Int
+i = 42
 
-d :: String
-d = "labas"
+ii :: Integer
+ii = 5634875683746583645873648563485763487563487563478
 
-ff' :: Bool
-ff' = True
+s :: String
+s = "labas"
+
+b' :: Bool
+b' = True
+
+f :: Float
+f = 3.8
+
+c :: Char
+c = 'd'
+
+d :: Double
+d = 44.333
+
+l :: [Int]
+l = []
+
+k :: [Int]
+k = 23 : l
+
+j :: [Int]
+j = 42 : k
+
+m :: [Int]
+m = 52 : l
+
+m' :: [Int]
+m' = 54 : 53 : 52 : []
+
+m'' :: [Int]
+m'' = [54, 53, 52]
+
+add :: Int -> Int -> Int
+add a b = a + b
+
+t :: (Int, String)
+t = (42, "")
+
+t' :: (Integer, Bool, Char)
+t' = (1, False, 'a')
+
+snd' :: (a, b, c) -> b
+snd' (_, v2, _) = v2
